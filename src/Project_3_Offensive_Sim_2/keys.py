51cd2bcf81aa2e38bd10d6d4fe4be72cc22ea62f
liam-sango <anonymous_aussie42@proton.me>
2026-06-07 15:39:41 +1000

    Add: implement key derivation using os.urandom and hmac in keys.py

---
+++ src/Project_3_Offensive_Sim_2/keys.py
@@ -0,0 +1,12 @@
+import os
+
+import hashlib
+import hmac
+
+#Derive our keys
+K_root = os.urandom(32)
+
+K_ratchet = hmac.new(K_root, b"RATCHET_INIT", hashlib.sha256)
+K_extract = hmac.new(K_root, b"EXTRACT", hashlib.sha256)
+
+
