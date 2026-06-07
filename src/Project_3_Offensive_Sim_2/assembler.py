92bd174b7fef60d0aae723296ba7111ee7d26b31
liam-sango <anonymous_aussie42@proton.me>
2026-06-07 17:38:44 +1000

    Add: format OPCODE_TABLE for better readability and update parse_bytecode_line function

--- src/Project_3_Offensive_Sim_2/assembler.py
+++ src/Project_3_Offensive_Sim_2/assembler.py
@@ -4,6 +4,7 @@ OPCODE_TABLE = {
     "DUP": 0x02,
     "SWAP": 0x03,
     "DROP": 0x04,
+
     # Arithmetic
     "ADD": 0x10,
     "SUB": 0x11,
@@ -11,17 +12,44 @@ OPCODE_TABLE = {
     "OR": 0x13,
     "XOR": 0x14,
     "NOT": 0x15,
+
     # Memory
     "LOAD32": 0x20,
     "STORE32": 0x21,
+
     # Control
     "JMP": 0x30,
     "JZ": 0x31,
     "JNZ": 0x32,
     "CALL": 0x33,
     "RET": 0x34,
+
     # System
     "SYSCALL": 0x40,
+
     # Halt
     "HALT": 0xFF,
 }
+
+
+def parse_bytecode_line(opcode):
+
+    if (opcode == "PUSH32"):
+        print("ABC")
+
+    if (opcode == "SYSCALL"):
+        print("ABC")
+
+    if (opcode == "JMP"):
+        print("DEF")
+    elif(opcode == "JZ"):
+        print("DEF")
+    elif (opcode == "JNZ"):
+        print("DEF")
+    elif (opcode == "CALL"):
+        print("DEF")
+    
+
+def assemble_payload(bytecode):
+    print("")
+
