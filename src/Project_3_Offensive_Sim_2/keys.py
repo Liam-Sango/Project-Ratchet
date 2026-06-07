412bbad1f1acc837a59a0ff2253e8892fea97429
liam-sango <anonymous_aussie42@proton.me>
2026-06-07 15:39:43 +1000

    Add: implement advance_ratchet function in keys.py and import keys in main.py

--- src/Project_3_Offensive_Sim_2/keys.py
+++ src/Project_3_Offensive_Sim_2/keys.py
@@ -9,4 +9,6 @@ K_root = os.urandom(32)
 K_ratchet = hmac.new(K_root, b"RATCHET_INIT", hashlib.sha256)
 K_extract = hmac.new(K_root, b"EXTRACT", hashlib.sha256)
 
+def advance_ratchet():
+    print('Hello World')
 
