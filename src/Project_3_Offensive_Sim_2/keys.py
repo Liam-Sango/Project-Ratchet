6265dfa96218e1df23a8967f52effd4ce3bca76a
liam-sango <anonymous_aussie42@proton.me>
2026-06-07 16:27:40 +1000

    Add: update advance_ratchet function to accept k_ratchet parameter and improve key derivation comments

--- src/Project_3_Offensive_Sim_2/keys.py
+++ src/Project_3_Offensive_Sim_2/keys.py
@@ -1,14 +1,16 @@
 import os
-
 import hashlib
 import hmac
 
-#Derive our keys
+#Derive our task persistent keys
 K_root = os.urandom(32)
-
 K_ratchet = hmac.new(K_root, b"RATCHET_INIT", hashlib.sha256)
 K_extract = hmac.new(K_root, b"EXTRACT", hashlib.sha256)
 
-def advance_ratchet():
-    print('Hello World')
+#Advanced ratchet mechanism
+def advance_ratchet(k_ratchet):
+    return hmac.new(k_ratchet, b"RATCHET", hashlib.sha256)
 
+#Derives our per task keys
+def derive_cmd_key(k_ratchet, salt):
+    K_cmd = hmac.new(k_ratchet, salt, b"CMDKEY")
