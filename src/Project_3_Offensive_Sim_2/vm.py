30f4132041a10265744ace78957f05df49363cb5
liam-sango <anonymous_aussie42@proton.me>
2026-06-17 13:25:10 +1000

    Refactor: adjust indentation in VirtualMachine class and define run() method structure

--- src/Project_3_Offensive_Sim_2/vm.py
+++ src/Project_3_Offensive_Sim_2/vm.py
@@ -1,6 +1,6 @@
 class VirtualMachine:
        
-       def __init__(self, bytecode, memory_size=4096):
+    def __init__(self, bytecode, memory_size=4096):
            
            if not isinstance(memory_size, int):
                raise ValueError("Memory size must be an integer")
@@ -20,13 +20,25 @@ class VirtualMachine:
            self.memory = bytearray(memory_size)
            self.instruction_pointer = 0
            self.is_halted = False
+   
+    def run(self):
 
+        while not self.is_halted and self.instruction_pointer < len(self.bytecode):
 
+            opcode = self.bytecode[self.instruction_pointer]
+            self.instruction_pointer += 1
+
+            #HALT
+            if opcode == 0xFF:
+                self.is_halted = True
+            else: 
+                raise ValueError (f"Unknown opcode: {opcode}")
 
 
 def execute_bytecode(bytecode: bytearray | bytes, memory_size : int = 4096):
 
     vm = VirtualMachine(bytecode, memory_size)
+    vm.run()
 
     return vm
     
