1ed4a305341c6f3560e09931b782c67a9f074933
liam-sango <anonymous_aussie42@proton.me>
2026-06-22 08:52:52 +1000

    refactor(arweave): clean up mock interface and add gateway stubs

--- src/Project_3_Offensive_Sim_2/arweave_interface.py
+++ src/Project_3_Offensive_Sim_2/arweave_interface.py
@@ -1,38 +1,37 @@
 import requests
 import uuid
-#Arweave gateways
+
 GATEWAYS = ["https://arweave.net", "https://ar-io.net"]
 
 class MockArweave:
-
-    #INIT
     def __init__(self):
         self.store = {}
     
+    #Stores a mock transaction in self.store
     def upload_image(self, image_path):
-        #Opens the image we wish to "upload"
+    
         with open(image_path, 'rb') as file:
             image_data = file.read()
-
-            #Stores our "uploaded" image in self.store
+        
             txid = uuid.uuid4().hex
             self.store[txid] = image_data
 
-            #Returns our TXID
             return txid
     
+    #Returns the stored image from self.store
     def download_image(self, txid):
-        #Returns the stored bytes from self.store
         if txid in self.store:
             stored_bytes = self.store[txid]
             return stored_bytes
         else:
-            #Raises error if TXID is not present.
             raise KeyError ("TXID is not present in self.store")
-        
 
+  
+def download_image(txid, gateway=None):
+    print("TEMP")
 
-
+def upload_image(wallet, image_path):
+    print("TEMP")
     
     
 
