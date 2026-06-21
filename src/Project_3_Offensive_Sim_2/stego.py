cd71e3c810a4cbdbf229e5648255c2e3f9219d54
liam-sango <anonymous_aussie42@proton.me>
2026-06-21 10:53:12 +1000

    fix(stego): implement bits-to-bytes conversion function

--- src/Project_3_Offensive_Sim_2/stego.py
+++ src/Project_3_Offensive_Sim_2/stego.py
@@ -13,7 +13,18 @@ def bytes_to_bits(data: bytes) -> list[int]:
     return bit_list
 
 def bits_to_bytes(bits: list[int]) -> bytes:
-    print("TEMP")
+    byte_values = []
+
+    for x in range(len(bits) // 8):
+         byte_chunk = 0
+
+         for i in range(8):
+            byte_chunk = (byte_chunk << 1) | bits[(x * 8) + i]
+
+         byte_values.append(byte_chunk)
+
+    finished_bytes = bytes(byte_values)
+    return finished_bytes
 
 def generate_positions(image_shape, k_extract: bytes, num_positions: int) -> list[tuple]:
     print ("TEMP")
