ebc881d75ad8c44958b2c1209eeaccafdafc13aa
liam-sango <anonymous_aussie42@proton.me>
2026-06-23 12:23:28 +1000

    refactor(project-3): pass parsed args to server and agent handlers

--- src/Project_3_Offensive_Sim_2/main.py
+++ src/Project_3_Offensive_Sim_2/main.py
@@ -13,7 +13,6 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-
 #Creates CLI using argparse
 
 #Main parser
@@ -21,40 +20,38 @@ import argparse
 parser = argparse.ArgumentParser(prog='MAIN', usage="WILL BE FILLED OUT", description="WILL BE FILLED OUT")
 subparser = parser.add_subparsers(dest="Command")
 
-
 #Server subcommands
 server_parser = subparser.add_parser(name="server", description="server commands", prog="MAIN")
 
-#Required 
 #--TASK
 server_parser.add_argument("--task", required=True, help="Space seperated assembly")
 #--COVER
 server_parser.add_argument("--cover", required=True, help="Path to the cover image")
-
-#Flags
 #--MOCK
 server_parser.add_argument("--mock", action="store_true", help="Use mock Arweave instead of real network")
-
 
 #Agent subcommands
 agent_parser  = subparser.add_parser(name="agent", description="agent commands", prog="MAIN")
 
-#Required 
 #--TXID
 agent_parser.add_argument("--txid", required=True, help="The TXID")
-
-#Flags
 #--MOCK
 agent_parser.add_argument("--mock", action="store_true", help="Use mock Arweave instead of real network")
 
 
-from src.Project_3_Offensive_Sim_2 import keys
+#Fill out later
+from src.Project_3_Offensive_Sim_2.assembler import assemble_payload
+from src.Project_3_Offensive_Sim_2.crypto_wrapper import encrypt_task, decrypt_task
+from src.Project_3_Offensive_Sim_2.stego import embed, extract
+from src.Project_3_Offensive_Sim_2.vm import execute_bytecode
+from src.Project_3_Offensive_Sim_2.arweave_interface import MockArweave
 
-def run_server():
+def run_server(args):
     print("TEMP")
 
-def run_agent():
+def run_agent(args):
     print("TEMP")
+
 
 # Parses args and dispatch
 if __name__ == "__main__":
