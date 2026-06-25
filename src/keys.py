b52af04e02e93bf28c1c5a1c81d5b55791e9e89f
liam-sango <anonymous_aussie42@proton.me>
2026-06-25 10:52:05 +1000

    refactor(keys): restructure key management into server, agent, and wallet functions

--- src/keys.py
+++ src/keys.py
@@ -1,11 +1,10 @@
 import os
 import hashlib
 import hmac
+import json
+import arweave
 
-#Derive our task persistent keys
-K_root = os.urandom(32)
-K_ratchet = hmac.new(K_root, b"RATCHET_INIT", hashlib.sha256).digest()
-K_extract = hmac.new(K_root, b"EXTRACT", hashlib.sha256).digest()
+#Shared functions
 
 #Advanced ratchet mechanism
 def advance_ratchet(k_ratchet):
@@ -14,3 +13,38 @@ def advance_ratchet(k_ratchet):
 #Derives our per task keys
 def derive_cmd_key(k_ratchet, salt):
     return hmac.new(k_ratchet, salt + b"CMDKEY", hashlib.sha256).digest()
+
+
+#Server side functions
+
+def server_generate_k_root():
+    k_root = os.urandom(32)
+    return k_root
+
+def server_derive_allkeys(k_root):
+    print("ABC")
+
+def server_save_server_keys(keyfile_path, k_root):
+    print("ABC")
+
+def server_load_server_keys(keyfile_path):
+    print("ABC")
+
+
+#Agent side functions
+
+def agent_save_agent_keys(keyfile_path, k_ratchet, K_exfil_ratchet, k_extract):
+    print("ABC")
+
+def load_agent_keys(keyfile_path):
+    print("ABC")
+
+
+#Wallet functions
+
+def load_wallet(keyfile_path):
+    print("ABC")
+
+
+
+
